d2258c66488bb3d29f515ae8fe5456dc821e726d
declantyson <declantyson@hotmail.co.uk>
2017-02-10 17:49:21 +0000

    The million dollar commit....?

--- GameplayScripts/Hylian_GameplayScripts.lua
+++ GameplayScripts/Hylian_GameplayScripts.lua
@@ -15,7 +15,7 @@ function EraChanged(playerId, EraId)
 
 	if playerConfig:GetCivilizationTypeName() == "CIVILIZATION_HYLIAN" then
 --		pPlayer:GetUnits():Create("UNIT_HERO_OF_TIME", capitalCity:GetX(), capitalCity:GetY())
-		pPlayer:GetUnits():Create("UNIT_SWORDSMAN", capitalCity:GetX(), capitalCity:GetY())
+		pPlayer:GetUnits():Create(GameInfo.Units.UNIT_HERO_OF_TIME.ID, capitalCity:GetX(), capitalCity:GetY())
 	end
 end
 
